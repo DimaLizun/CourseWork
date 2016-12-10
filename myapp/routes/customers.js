4c96c701a38d55fe9dd617b3b8f85dfffff1f275
Dima Lizun <dimalizun@gmail.com>
2016-12-11 02:27:35 +0300

    add template view for customers

--- myapp/routes/customers.js
+++ myapp/routes/customers.js
@@ -4,7 +4,7 @@ exports.list = function (req,res) {
         var query = connection.query('SELECT * FROM customers', function (err, rows) {
             if (err)
                 console.log("ERROR" + err);
-            res.render('customers',{page_title:"Customers - Node.js", data:rows});
+            res.render('table',{page_title:"Customers - Node.js", data:rows});
         });
     });
 }
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -4,7 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-  res.render('table');
+
 });
+
+router.get('/table',function (req,res) {
+   res.render('table',{title: 'table'})
+})
 
 module.exports = router;
